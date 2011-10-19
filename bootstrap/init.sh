916b03ee30e69d5b6d9809fbe5f79ae4a9f69ac7
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-19 21:57:35 +0200

    bootstrap: extend stage1 init

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -2,14 +2,18 @@
 export PATH=/stage1
 
 busybox cd /
-busybox rm init
+busybox date >>boot.txt
+exec >>boot.txt 2>&1
+busybox rm -fr init
+busybox mkdir -p /proc
+busybox mkdir -p /sys
 busybox mount -t proc proc /proc
 busybox mount -t sysfs sysfs /sys
 
 RAMDISK=ramdisk.cpio.gz
 
-if busybox grep -q bootmode=2 /proc/cmdline ; then
-	# recovery boot
+if busybox grep -q bootmode=2 /proc/cmdline || busybox grep -q androidboot.mode=reboot_recovery /proc/cmdline ; then
+    # recovery boot
     RAMDISK=ramdisk-recovery.cpio.gz
 fi
 
@@ -17,9 +21,11 @@ busybox gunzip -c ${RAMDISK} | busybox cpio -i
 
 busybox umount /sys
 busybox umount /proc
+busybox date >>boot.txt
+
+busybox rm -fr ramdisk.cpio.gz
+busybox rm -fr ramdisk-recovery.cpio.gz
 
 busybox rm -fr /stage1 /dev/*
-busybox rm ramdisk.cpio.gz
-busybox rm ramdisk-recovery.cpio.gz
 
 exec /init
