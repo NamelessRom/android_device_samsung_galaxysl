6abc55c84effe406ebc4ebf04187722db9136954
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-19 19:33:17 +0200

    fix shbootimg

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -6,11 +6,11 @@ busybox rm init
 busybox mount -t proc proc /proc
 busybox mount -t sysfs sysfs /sys
 
-RAMDISK=ramdisk.cpio.gz
+RAMDISK=ramdisk.img
 
 if busybox grep -q bootmode=2 /proc/cmdline ; then
 	# recovery boot
-    RAMDISK=ramdisk-recovery.cpio.gz
+    RAMDISK=ramdisk-recovery.img
 fi
 
 busybox gunzip -c ${RAMDISK} | busybox cpio -i
--- shbootimg.mk
+++ shbootimg.mk
@@ -1,13 +1,15 @@
 LOCAL_PATH := $(call my-dir) 
 
-MASTER_BOOTSTRAP := $(PRODUCT_OUT)/bootstrap.img
+MASTER_BOOTSTRAP := $(PRODUCT_OUT)/ramdisk.cpio.gz
 $(MASTER_BOOTSTRAP): $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk) $(HOST_OUT)/bin/mkbootfs $(PRODUCT_OUT)/utilities/unpackbootimg $(PRODUCT_OUT)/utilities/busybox
 	$(call pretty,"Bootstrap: $@")
 	cp -r device/samsung/galaxysl/bootstrap $(PRODUCT_OUT)
-	cp $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/bootstrap/
 	cp $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/bootstrap/
 	cp $(recovery_ramdisk) $(PRODUCT_OUT)/bootstrap/
-	./$(HOST_OUT)/bin/mkbootfs $(PRODUCT_OUT)/bootstrap > $@
+
+	pushd $(PRODUCT_OUT)/bootstrap
+	find . | cpio -o -H newc | gzip > ../ramdisk.cpio.gz
+	popd
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(MASTER_BOOTSTRAP)
