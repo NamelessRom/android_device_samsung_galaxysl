8fdeaf8ebbf5ca952c57dd8a61a4a088659172de
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-19 19:29:02 +0200

    fix bootstrap

---
+++ bootstrap/init.sh
@@ -0,0 +1,25 @@
+#!/stage1/busybox sh
+export PATH=/stage1
+
+busybox cd /
+busybox rm init
+busybox mount -t proc proc /proc
+busybox mount -t sysfs sysfs /sys
+
+RAMDISK=ramdisk.cpio.gz
+
+if busybox grep -q bootmode=2 /proc/cmdline ; then
+	# recovery boot
+    RAMDISK=ramdisk-recovery.cpio.gz
+fi
+
+busybox gunzip -c ${RAMDISK} | busybox cpio -i
+
+busybox umount /sys
+busybox umount /proc
+
+busybox rm -fr /stage1 /dev/*
+busybox rm ramdisk.cpio.gz
+busybox rm ramdisk-recovery.cpio.gz
+
+exec /init
