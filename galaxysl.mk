ed494912eed3f6bbee09fbc9bd72d0f1a031ab3f
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-01-15 02:42:05 +0100

    added modified ics init.rc to make it booting

--- galaxysl.mk
+++ galaxysl.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES := \
 
 # Init files
 PRODUCT_COPY_FILES += \
+	device/samsung/galaxysl/init.rc:root/init.rc \
 	device/samsung/galaxysl/init.latona.rc:root/init.latona.rc \
 	device/samsung/galaxysl/lpm.rc:root/lpm.rc \
 	device/samsung/galaxysl/lpm.rc:recovery/root/lpm.rc \
