b8c9171eed65c3324be2d26c8f78ba7a3785d26c
Dheeraj CVR <cvr.dheeraj@gmail.com>
2012-12-01 15:05:50 +0530

    latona: get rid of bootstrap script

--- shbootimg.mk
+++ shbootimg.mk
@@ -4,8 +4,9 @@ MASTER_BOOTSTRAP := $(PRODUCT_OUT)/bootstrap.cpio.gz
 $(MASTER_BOOTSTRAP): $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk) $(HOST_OUT)/bin/mkbootfs $(PRODUCT_OUT)/utilities/busybox
 	$(call pretty,"Bootstrap: $@")
 	cp -r device/samsung/galaxysl/bootstrap $(PRODUCT_OUT)
-	cp -r device/samsung/galaxysl/bootstrap.sh $(PRODUCT_OUT)
-	$(PRODUCT_OUT)/bootstrap.sh
+	cd out/target/product/galaxysl/root/; chmod 0644 *.rc default.prop; find . | cpio -o -H newc | gzip > ../bootstrap/ramdisk.cpio.gz
+	cd out/target/product/galaxysl/recovery/root/; chmod 0644 *.rc default.prop; find . | cpio -o -H newc | gzip > ../../bootstrap/ramdisk-recovery.cpio.gz
+	cd out/target/product/galaxysl/bootstrap/; find . | cpio -o -H newc | gzip > ../bootstrap.cpio.gz
 
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 $(INSTALLED_BOOTIMAGE_TARGET): $(TARGET_PREBUILT_KERNEL) $(MASTER_BOOTSTRAP)
