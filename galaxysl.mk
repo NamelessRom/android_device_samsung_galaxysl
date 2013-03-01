0d486e029f618720e6727f49b68985668c3478d8
Dheeraj CVR <cvr.dheeraj@gmail.com>
2013-03-01 17:04:52 +0530

    latona: device specific dalvik changes

--- galaxysl.mk
+++ galaxysl.mk
@@ -214,21 +214,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
         ro.com.google.locationfeatures=1 \
         ro.com.google.networklocation=1
 
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += \
-        dalvik.gc.type-precise
-
 # Our cache partition isn't big enough for dalvik-cache.
 PRODUCT_PROPERTY_OVERRIDES += \
         dalvik.vm.dexopt-data-only=1
 
-# Extended JNI checks
-# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs
-# before they have a chance to cause problems.
-# Default=true for development builds, set by android buildsystem.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.checkjni=0 \
-    dalvik.vm.checkjni=false
 
 # Override /proc/sys/vm/dirty_ratio on UMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -247,7 +236,7 @@ include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
 # the the build-time selection of resources). The product definitions including
 # this file must pay attention to the fact that the first entry in the final
 # PRODUCT_LOCALES expansion must not be a density.
-PRODUCT_LOCALES := hdpi
+PRODUCT_AAPT_CONFIG := normal hdpi
 
 # copy the filesystem converter
 PRODUCT_COPY_FILES += \
