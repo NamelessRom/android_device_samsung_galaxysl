c049e3f881678796cdd81d9ddefeea977be23420
Dheeraj CVR <cvr.dheeraj@gmail.com>
2013-05-28 11:32:14 +0530

    bootstrap: fixup issues with mountpoints
    Thanks to sconosciuto

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,4 +1,5 @@
 #!/stage1/busybox sh
+_PATH="$PATH"
 export PATH=/stage1
 
 busybox cd /
@@ -9,44 +10,44 @@ busybox mount -t proc proc /proc
 busybox mount -t sysfs sysfs /sys
 
 busybox mount -t yaffs2 /dev/block/mtdblock2 /system
-busybox mount -t ext4 /dev/block/mmcblk0p2 /cache
 
 RAMDISK=ramdisk.cpio.gz
 
-if busybox test -e /cache/.startrecovery || busybox grep -q bootmode=2 /proc/cmdline || busybox grep -q androidboot.mode=reboot_recovery /proc/cmdline ; then
+if busybox grep -q bootmode=2 /proc/cmdline || busybox grep -q androidboot.mode=reboot_recovery /proc/cmdline ; then
 	# recovery boot
-	busybox rm -fr /cache/.startrecovery
 	RAMDISK=ramdisk-recovery.cpio.gz
 
 elif ! busybox test -e /system/build.prop ; then
 	# emergency boot
-	busybox umount /cache
+
+	# wait for the internal memory and the external SD
+	busybox sleep 3
+
 	make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /cache /dev/block/mmcblk0p2
 	busybox mount -t ext4 /dev/block/mmcblk0p2 /cache
 	busybox mkdir /cache/recovery
 
+	busybox mkdir -p /emmc
+	busybox mkdir -p /sdcard
 	busybox mount -t vfat /dev/block/mmcblk0p1 /emmc
+	busybox mount -t vfat /dev/block/mmcblk1p1 /sdcard
 
 	UPDATE=$(busybox cat /emmc/cyanogenmod.cfg)
 
-	if [[ -n "$UPDATE" ]]; then
+	if busybox test -n "$UPDATE" && busybox test -e "$UPDATE"; then
 		busybox echo "install_zip(\"`echo $UPDATE`\");" > /cache/recovery/extendedcommand
 	fi
 
 	RAMDISK=ramdisk-recovery.cpio.gz
-
-	busybox umount /sdcard
-	busybox umount /emmc
 fi
 
-busybox umount /cache
 busybox umount /system
 
 busybox gunzip -c ${RAMDISK} | busybox cpio -i
 
 if busybox grep -q bootmode=5 /proc/cmdline || busybox grep -q androidboot.mode=usb_charger /proc/cmdline ; then
-    # charging mode
-    busybox cp lpm.rc init.rc
+	# charging mode
+	busybox cp lpm.rc init.rc
 	busybox rm init.latona.rc
 fi
 
@@ -59,4 +60,5 @@ busybox rm -fr ramdisk-recovery.cpio.gz
 
 busybox rm -fr /stage1 /dev/*
 
+export PATH="${_PATH}"
 exec /init
